3375528ee219d782b5bf133540f437cb662b0262
User <User@DESKTOP-519F49N>
2022-11-25 23:51:50 -0500

    semana12 listas enlazadas

---
+++ src/semana12_listas/ListaOrdenada.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana12_listas;
+
+/**
+ *
+ * @author User
+ */
+public class ListaOrdenada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    void insertaOrden(int d) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void visualizar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/semana12_listas/Nodo.java
@@ -0,0 +1,29 @@
+
+package semana12_listas;
+
+/**
+ *
+ * @author Zuasnabar Paucar Madeleybi
+ */
+public class Nodo 
+{ 
+int dato; 
+Nodo enlace;
+public Nodo(int x)
+{
+ dato = x;
+ enlace = null;
+}
+public int getDato()
+{
+ return dato;
+}
+public Nodo getEnlace()
+{
+ return enlace;
+}
+public void setEnlace(Nodo enlace)
+{
+ this.enlace = enlace;
+}
+}
